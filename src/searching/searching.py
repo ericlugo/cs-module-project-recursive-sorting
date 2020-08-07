61a0bafad1811914d61f47b8d0793048100f41f1
Eric SarragaLugo <ericlugo@users.noreply.github.com>
2020-08-07 02:17:53 -0400

    searching mvp and stretch complete. on to sorting.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,51 @@
-# TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    if start <= end:
+        middle = (start + end) // 2
+        if arr[middle] == target:
+            return middle
+        elif target < arr[middle]:
+            return binary_search(arr, target, start, middle-1)
+        else:
+            return binary_search(arr, target, middle+1, end)
+    else:
+        return -1
 
+# RECURSIVE IMPLEMENTATION:
+def recursive_agnostic_binary_search(arr, target, start, end):
+    if start <= end:
+        middle = (start + end) // 2
+        if arr[middle] == target:
+            return middle
+        elif arr[start] < arr[end]:
+            if target < arr[middle]:
+                return recursive_agnostic_binary_search(arr, target, start, middle-1)
+            else:
+                return recursive_agnostic_binary_search(arr, target, middle+1, end)
+        else:
+            if arr[middle] < target:
+                return recursive_agnostic_binary_search(arr, target, start, middle-1)
+            else:
+                return recursive_agnostic_binary_search(arr, target, middle+1, end)
+    else:
+        return -1
 
-# STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
-# the target regardless of whether the input array is
-# sorted in ascending order or in descending order
-# You can implement this function either recursively 
-# or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
-
+# ITERATIVE IMPLEMENTATION:
+def iterative_agnostic_binary_search(arr, target):
+    if 0 < len(arr):
+        start = 0
+        end = len(arr) - 1
+        while start <= end:
+            middle = (start + end) // 2
+            if arr[middle] == target:
+                return middle
+            elif arr[start] < arr[end]:
+                if target < arr[middle]:
+                    end = middle-1
+                elif arr[middle] < target:
+                    start = middle+1
+            else:
+                if arr[middle] < target:
+                    end = middle-1
+                elif target < arr[middle]:
+                    start = middle+1
+    return -1
