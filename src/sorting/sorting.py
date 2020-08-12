aadd81af391c8f8a54fbc466a73108b9fa85e83c
Eric SarragaLugo <ericlugo@users.noreply.github.com>
2020-08-11 21:18:31 -0400

    MVP and Stretch complete

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,48 +1,85 @@
+"""
+merge(LHS, RHS):
+    initiate a list to store values in sorted order
+    while both lists have values comparison will be necessary
+        append smallest value to sorted list and remove from comparison
+    append any values left behind after comparisons
+    only one of these cases should be valid after the prior step
+    return the merged sorted values
+"""
+
 def merge(LHS, RHS):
-    # initiate a list to store values in sorted order
     merged_list = []
-    # while both lists have values comparison will be necessary
     while (len(LHS) > 0) and (len(RHS) > 0):
-        # append smallest value to sorted list and remove from comparison
         if LHS[0] < RHS[0]:
             merged_list.append(LHS[0])
             LHS.pop(0)
         else:
             merged_list.append(RHS[0])
             RHS.pop(0)
-    # append any values left behind after comparisons
-    # only one of these cases should be valid after the prior step
     for each_value in LHS:
         merged_list.append(each_value)
     for each_value in RHS:
         merged_list.append(each_value)
-    # return the merged sorted values
     return merged_list
 
+"""
+merge_sort(arr):
+    multi-element list is case for recursion
+        split the list into halves
+        recursively split the list until base case is reached
+        return a sorted list using the merge helper to join the two halves 
+    single element list, which --by definition-- cannot be unsorted, is base case
+"""
 def merge_sort(arr):
-    # multi-element list is case for recursion
     if len( arr ) > 1:
-        # split the list into halves
         LHS = arr[:len(arr)//2]
         RHS = arr[len(arr)//2:]
-        # recursively split the list until base case is reached
         LHS = merge_sort(LHS)
         RHS = merge_sort(RHS)
-        # return a sorted list using the merge helper to join the two halves 
         return merge(LHS, RHS)
-    # single element list, which --by definition-- cannot be unsorted, is base case
     return arr
 
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
-# utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
-# or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
-    pass
+"""
+merge_in_place(arr, start_index, middle_index, end_index):
+    if already sorted simply return
+    while each sublist still has elements in it
+        if arr[start_index] is already in right position simply move the left pointer
+        otherwise:
+            store value and index for current start of second list
+            shift all elements between the start of the first and second sublists to the right by 1
+            insert the stored value in the correct position
+            update all pointers
+"""
+def merge_in_place(arr, start_index, middle_index, end_index):
+    start_index_2 = middle_index+1
+    if arr[middle_index] <= arr[start_index_2]:
+        return
+    while (start_index <= middle_index) and (start_index_2 <= end_index):
+        if arr[start_index] <= arr[start_index_2]:
+            start_index += 1
+        else:
+            temporary_value = arr[start_index_2]
+            temporary_index = start_index_2
+            while temporary_index != start_index:
+                arr[temporary_index] = arr[temporary_index-1]
+                temporary_index -= 1
+            arr[start_index] = temporary_value
+            start_index += 1
+            middle_index += 1
+            start_index_2 += 1
 
-
-def merge_sort_in_place(arr, l, r):
-    # Your code here
-    pass
+"""
+merge_sort_in_place(arr, left_index, right_index):
+    multi-element list is the case for recursion. if base case evaluate as False and begin the merging process
+        prevents overflow for large left or right index values
+        create more pointers to mark each subsegment of the array before beginning the merge
+        sort the sublists using the created markers
+"""
+def merge_sort_in_place(arr, left_index, right_index):
+    if left_index < right_index:
+        middle_index = left_index + (right_index-left_index) // 2
+        merge_sort_in_place(arr, left_index, middle_index)
+        merge_sort_in_place(arr, middle_index+1, right_index)
+        merge_in_place(arr, left_index, middle_index, right_index)
